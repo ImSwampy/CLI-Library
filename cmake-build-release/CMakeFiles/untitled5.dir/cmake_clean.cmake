0a11b17143ff44d30f612c61fe3fde99faea7f29
ImSwampy <tijaguar44@gmail.com>
2024-08-31 23:06:33 +0200

    idk what i added but i did added

---
+++ cmake-build-release/CMakeFiles/untitled5.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/untitled5.dir/main.cpp.obj"
+  "CMakeFiles/untitled5.dir/main.cpp.obj.d"
+  "CMakeFiles/untitled5.dir/terminal/input/checkbox/checkbox.cpp.obj"
+  "CMakeFiles/untitled5.dir/terminal/input/checkbox/checkbox.cpp.obj.d"
+  "CMakeFiles/untitled5.dir/terminal/input/choice/choice.cpp.obj"
+  "CMakeFiles/untitled5.dir/terminal/input/choice/choice.cpp.obj.d"
+  "CMakeFiles/untitled5.dir/terminal/terminal.cpp.obj"
+  "CMakeFiles/untitled5.dir/terminal/terminal.cpp.obj.d"
+  "libuntitled5.dll.a"
+  "untitled5.exe"
+  "untitled5.exe.manifest"
+  "untitled5.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/untitled5.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
