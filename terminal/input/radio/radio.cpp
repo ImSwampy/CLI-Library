800aa57e418a0466b6da3eb1d8d04621df449094
Joseph Alba <josephalba468@gmail.com>
2024-09-05 22:42:31 +0100

    Fixed bounds checking.

--- terminal/input/radio/radio.cpp
+++ terminal/input/radio/radio.cpp
@@ -9,7 +9,7 @@ void Radio::add_choice(Choice &choice) {
 }
 
 void Radio::select() {
-    if (0 < hovered < choices.size()-1) {
+    if (hovered < choices.size()) {
         if (got_selected != -1) choices[got_selected].select();
         choices[hovered].select();
         got_selected = hovered;
@@ -18,8 +18,8 @@ void Radio::select() {
     }
 }
 
-void Radio::select(int choice_index) {
-    if (0 < choice_index < choices.size()-1) {
+void Radio::select(unsigned short choice_index) {
+    if (choice_index < choices.size()) {
         if (got_selected != -1) choices[got_selected].select();
         choices[choice_index].select();
         got_selected = hovered;
@@ -64,7 +64,7 @@ void Radio::change_hover(hover_opt option) {
 }
 
 std::optional<Choice> Radio::get_choice(int choice_index) {
-    if (0 < choice_index < choices.size()) {
+    if (choice_index < choices.size()) {
         return choices[choice_index];
     }
     return {};
