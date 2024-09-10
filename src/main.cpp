6b55ad0995a5adbf69b40c5bd508c17ceac8d942
ImSwampy <tijaguar44@gmail.com>
2024-09-10 14:54:49 +0200

    print choice

--- src/main.cpp
+++ src/main.cpp
@@ -81,7 +81,7 @@ int main() {
 
     terminal.clear();
 
-    terminal.print(radio.get_selected()->get_name());
+    terminal.print(terminal.get_choice()->get_name());
     std::cin.get();
     /*
     auto res = terminal.get_choice(radio);
