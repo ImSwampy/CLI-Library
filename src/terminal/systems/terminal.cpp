791cbb800baccc27e6332de919837f46c3723041
ImSwampy <83744625+ImSwampy@users.noreply.github.com>
2024-09-17 22:56:32 +0200

    fixed inline functions

--- src/terminal/systems/terminal.cpp
+++ src/terminal/systems/terminal.cpp
@@ -7,7 +7,7 @@
 
 #define EXTENDED_KEY 224
 
-inline void Terminal::clear() {
+void Terminal::clear() {
     system("cls");
 }
 
@@ -73,7 +73,3 @@ Keys Terminal::detect_kb_input(bool) {
 }
 
 #endif
-
-inline void Terminal::clear() {
-    print("\033c");
-}
